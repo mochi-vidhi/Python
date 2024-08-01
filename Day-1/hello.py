8b2eef628c1b5cda040cb91d2edbc125bab20e1b
Mochi Vidhi <148563893+mochi-vidhi@users.noreply.github.com>
2024-08-01 18:16:33 +0000

    python-basics

---
+++ Day-1/hello.py
@@ -0,0 +1,8 @@
+print("Hello world!!")
+# This is comment in python 
+"""
+This is not multi line comments but doc string in python
+"""
+'''
+This is also doc string in python.
+'''
---
+++ Day-1/input.py
@@ -0,0 +1,10 @@
+# name = 10
+# print(name)
+# print(type(name))
+
+# implicit type conversion. 
+# age = int(input("Enter the age:"))
+age = input("Enter Your age:")
+# age = int(age) # explicit type conversion
+print(type(int(age))) # explicit type conversion 
+print(int(age)) 
